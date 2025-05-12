ab9ebf13f284e73f357045c7a28d14ffe239fac2
Tarun Rohit <rohittarun9@gmail.com>
2025-05-12 17:34:28 +0530

    fixed errors

--- backend/app.py
+++ backend/app.py
@@ -74,5 +74,6 @@ def upload_image():
     else:
         return jsonify({'error': 'Invalid file type. Please upload a PNG or JPG image.'}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+port = int(os.environ.get("PORT", 5001))
+app.run(host='0.0.0.0', port=port)
+
