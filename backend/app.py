8c98c90ed23d0f21d0dde1f1288dfecd240d5a0f
Tarun Rohit <rohittarun9@gmail.com>
2025-05-13 10:35:53 +0530

    i have added notebooks

--- backend/app.py
+++ backend/app.py
@@ -74,6 +74,5 @@ def upload_image():
     else:
         return jsonify({'error': 'Invalid file type. Please upload a PNG or JPG image.'}), 400
 
-port = int(os.environ.get("PORT", 5001))
-app.run(host='0.0.0.0', port=port)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
